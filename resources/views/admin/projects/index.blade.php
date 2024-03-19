cb45dfc90a7c2c96ddcde9585116bca65fffff07
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-03-19 21:37:08 +0100

    added content in index.blade.php

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,33 +8,40 @@
         <h1>Progetti</h1>
     </header>
 
-    <table class="table">
+    <table class="table table-striped">
         <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Creato il</th>
+                <th scope="col">Ultima modifica</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
+        @forelse($projects as $project)
             <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <th scope="row">{{$project->id}}</th>
+                <td>{{$project->title}}</td>
+                <td>{{$project->slug}}</td>
+                <td>{{$project->created_at}}</td>
+                <td>{{$project->updated_at}}</td>
+                <td>
+                    <div class="d-flex justify-content-end">
+                        <a href="{{route('admin.projects.show', $project)}}" class="btn btn-sm btn-primary">
+                            <i class="fa-solid fa-eye"></i>
+                        </a>
+                    </div>
+                </td>
             </tr>
+        @empty 
             <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                <td colspan="6">
+                    <h3 class="text-center">Non ci sono progetti</h3>
+                </td>
             </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-            </tr>
+        @endforelse
         </tbody>
     </table>
 
